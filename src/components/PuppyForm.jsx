74b868d10245c95e0be486f1ce5ad329d50fde1c
Katie Raby <katie.raby@icloud.com>
2020-03-05 11:18:39 +0000

    Set up for form

--- src/components/PuppyForm.jsx
+++ src/components/PuppyForm.jsx
@@ -5,6 +5,17 @@ const PuppyForm = () => {
     <div className="formContainer">
       <form className="PuppyForm">
         <h2>Add a pupper to the list!</h2>
+        {/* {//need name, url and cuteness in an obj to add to array */}
+        <label>
+          Name:<input type="text" placeholder="Enter name"></input>
+        </label>
+        <label>
+          Image URL: <input type="text" placeholder="Enter image URL"></input>
+        </label>
+        <label>
+          Cuteness level:
+          <input type="number" placeholder="Enter cuteness level"></input>
+        </label>
       </form>
     </div>
   );
